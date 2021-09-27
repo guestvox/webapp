9f5ed1d13a330fd289448e46184194bfb561e2db
Gersón Gómez <gergomez18@gmail.com>
2021-09-27 15:20:39 -0500

    Actualización

--- core/api/Orders_api.php
+++ core/api/Orders_api.php
@@ -6,24 +6,56 @@ class Orders_api extends Model
     {
         if (!empty($params[0]))
         {
-            if (!empty($params[1]))
+            if ($params[1] == 'all' OR $params[1] == 'declined' OR $params[1] == 'accepted' OR $params[1] == 'delivered')
             {
-                $query = Functions::get_json_decoded_query($this->database->select('menu_orders', '*', [
-                    'id' => $params[1],
+                $where = [
+                    'AND' => [
+                        'account' => $params[0]
+                    ],
                     'ORDER' => [
                         'id' => 'DESC'
                     ]
-                ]));
+                ];
 
-                return !empty($query) ? $query[0] : 'No se encontraron registros';
+                if ($params[1] == 'declined')
+                    $where['AND']['declined'] = true;
+                else if ($params[1] == 'accepted')
+                {
+                    $where['AND']['accepted'] = true;
+                    $where['AND']['delivered'] = false;
+                }
+                else if ($params[1] == 'delivered')
+                    $where['AND']['delivered'] = true;
+
+                $query = Functions::get_json_decoded_query($this->database->select('menu_orders', '*', $where));
+
+                if (!empty($query))
+                {
+                    foreach ($query as $key => $value)
+                    {
+                        if (!empty($value['owner']))
+                        {
+                            $value['owner'] = nctions::get_json_decoded_query($this->database->select('owners', '*', [
+                                'id' => $value['owner']
+                            ]));
+
+                            if (!empty($value['owner']))
+                                $query[$key]['owner'] = $value['owner'][0];
+                        }
+                    }
+                }
+                else
+                    return 'No se encontraron registros';
+
+                return !empty($query) ? $query : 'No se encontraron registros';
             }
             else
             {
                 $query = Functions::get_json_decoded_query($this->database->select('menu_orders', '*', [
-                    'account' => $params[0]
+                    'id' => $params[1]
                 ]));
 
-                return !empty($query) ? $query : 'No se encontraron registros';
+                return !empty($query) ? $query[0] : 'No se encontraron registros';
             }
         }
         else
