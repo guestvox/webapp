58aceb6258d0b1517f0e43d5f79f1b8600669a93
David Miguel Gómez Macías <davidgomezmacias@gmail.com>
2023-10-16 21:03:04 -0500

    Basic Settings

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  guestvox: {
+    apiUrl: "http://local.api.guestvox.com"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  guestvox: {
+    apiUrl: "https://api.guestvox.com"
+  }
+};
